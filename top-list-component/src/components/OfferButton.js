d5411d674892ae9b9ee8f37f11f33cc0ab30f5fc
Cristian Alan Carranza Alcantar <crisifur19@gmail.com>
2022-08-28 00:51:43 -0500

    Code button state adjusment, now the button change its style through use State

--- top-list-component/src/components/OfferButton.js
+++ top-list-component/src/components/OfferButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Stack from "react-bootstrap/esm/Stack";
 
 export default function OfferButton({
@@ -8,6 +8,8 @@ export default function OfferButton({
   code,
   affiliateLink,
 }) {
+  const [btnColor, setBtnColor] = useState("code-button");
+
   return (
     <Stack>
       <h5 className="bold secondary-font primary-color">{`[${brandName}] // [${productGroup}]`}</h5>
@@ -16,10 +18,13 @@ export default function OfferButton({
       <p className="secondary-font tiny">Terms &amp; Conditions Apply</p>
       <div className="spacer-sm"></div>
       <section className="dC">
-        <button className="code-button">
+        <button
+          className={btnColor}
+          onClick={() => setBtnColor("code-button-clicked")}
+        >
           <ul>
             <li>
-              <p className="primary-color secondary-font small bold">{code}</p>
+              <p className=" secondary-font small bold">{code}</p>
             </li>
             <li>
               <svg
@@ -33,7 +38,6 @@ export default function OfferButton({
                   fill-rule="evenodd"
                   clip-rule="evenodd"
                   d="M16.5 0H6.5C5.4 0 4.5 0.9 4.5 2V16C4.5 17.1 5.4 18 6.5 18H16.5C17.6 18 18.5 17.1 18.5 16V2C18.5 0.9 17.6 0 16.5 0ZM2.5 4H0.5V20C0.5 21.1 1.4 22 2.5 22H14.5V20H2.5V4ZM6.5 16H16.5V2H6.5V16Z"
-                  fill="#575757"
                 />
               </svg>
             </li>
